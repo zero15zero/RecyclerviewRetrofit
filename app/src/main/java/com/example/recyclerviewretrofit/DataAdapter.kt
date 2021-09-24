057b5c63a5fb1d1ae7fad3d865f55a73983908b2
Shivakant <shivakant.upadhyay@timesinternet.in>
2021-09-24 11:45:05 +0530

    Item click handled

--- app/src/main/java/com/example/recyclerviewretrofit/DataAdapter.kt
+++ app/src/main/java/com/example/recyclerviewretrofit/DataAdapter.kt
@@ -7,10 +7,14 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.util.ArrayList
 
-class DataAdapter(private val datas:ArrayList<String>) : RecyclerView.Adapter<Holder>() {
+class DataAdapter(private val datas:ArrayList<String>,private val listner:ItemClicked) : RecyclerView.Adapter<Holder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view=LayoutInflater.from(parent.context).inflate(R.layout.item,parent,false)
-        return Holder(view)
+        val viewHolder=Holder(view)
+        view.setOnClickListener{
+            listner.onItemClick(datas[viewHolder.adapterPosition])
+        }
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
@@ -25,4 +29,8 @@ class DataAdapter(private val datas:ArrayList<String>) : RecyclerView.Adapter<Ho
 
 class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val itemTitle : TextView=itemView.findViewById(R.id.textView)
+}
+
+interface ItemClicked{
+    fun onItemClick(item:String)
 }
--- app/src/main/java/com/example/recyclerviewretrofit/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewretrofit/MainActivity.kt
@@ -2,17 +2,18 @@ package com.example.recyclerviewretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ItemClicked {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView.layoutManager=LinearLayoutManager(this)
         val items=generateData()
-        val adapter=DataAdapter(items)
+        val adapter= DataAdapter(items,this)
         recyclerView.adapter=adapter
     }
     private fun generateData():ArrayList<String>
@@ -21,5 +22,9 @@ class MainActivity : AppCompatActivity() {
         for(i in 0 until 100)
             list.add("Item $i")
         return list
+    }
+
+    override fun onItemClick(item: String) {
+        Toast.makeText(this,"You clicked $item",Toast.LENGTH_LONG).show()
     }
 }
